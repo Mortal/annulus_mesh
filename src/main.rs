258173291532afb9bacfc3e6701b58c03b28f431
Mathias Rav <rav@cs.au.dk>
2017-02-23 10:35:28 -0500

    Weird double unused import warning

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,17 @@ extern crate mersenne_twister;
 
 mod annulus_distribution;
 pub mod blue_noise;
+mod qhull;
 
 use std::f64::consts::PI;
 #[allow(unused_imports)]
 use gnuplot::{Figure, Caption, Color, Fix, AxesCommon, PlotOption, DashType, Coordinate, TextColor};
 use rand::SeedableRng;
 use std::io::{Write, Cursor, BufRead};
-use std::process::{Command, Stdio};
 use docopt::Docopt;
 use mersenne_twister::MT19937_64;
-use std::ops::Add;
+pub use qhull::Point;
+use qhull::qhull_triangulation;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
@@ -49,71 +50,6 @@ struct Args {
     flag_r: f64,
     flag_no_show: bool,
     flag_plot: Option<String>,
-}
-
-/// A point in 2D.
-#[derive(Copy, Clone, Debug)]
-pub struct Point(f64, f64);
-
-impl Add for Point {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self {
-        Point(self.0 + rhs.0, self.1 + rhs.1)
-    }
-}
-
-impl Point {
-    /// Distance between two points.
-    fn dist(self, y: Point) -> f64 {
-        self.dist_sq(y).sqrt()
-    }
-
-    /// The square of the distance between two points.
-    fn dist_sq(self, y: Point) -> f64 {
-        (self.0 - y.0).powi(2) + (self.1 - y.1).powi(2)
-    }
-}
-
-/// Compute the Delaunay triangulation of a set of points by running them through `qhull`.
-///
-/// # Panics
-///
-/// Panics if `qhull` command is not available or fails.
-fn qhull_triangulation(ps: &[Point]) -> Vec<Vec<usize>> {
-    let mut process = Command::new("qhull")
-                          .stdin(Stdio::piped())
-                          .stdout(Stdio::piped())
-                          .arg("d")
-                          .arg("i")
-                          .spawn()
-                          .unwrap_or_else(|e| panic!("couldn't spawn qhull: {:?}", e));
-
-    if let Some(ref mut out) = process.stdin {
-        writeln!(out, "2\n{}", ps.len()).unwrap();
-
-        for &Point(x, y) in ps {
-            writeln!(out, "{} {}", x, y).unwrap();
-        }
-    }
-
-    // process.stdout.unwrap().read_to_string(&mut s);
-    let output = process.wait_with_output().unwrap();
-
-    let mut lines = Cursor::new(output.stdout).lines();
-
-    let n = lines.next().unwrap().unwrap().parse::<usize>().unwrap();
-
-    let mut res = vec![];
-
-    for line in lines {
-        let s = line.unwrap();
-        let poly: Vec<usize> = s.split_whitespace().map(|e| e.parse::<usize>().unwrap()).collect();
-        res.push(poly);
-    }
-
-    assert_eq!(res.len(), n);
-    res
 }
 
 fn main() {
